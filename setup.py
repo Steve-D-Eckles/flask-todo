0c55d2ef62c25cacf65ea801caf9c175a56aaf5e
Kevin Lueke <kevinlueke66@gmail.com>
2020-03-30 14:07:40 -0400

    Edit setup.py for personal info

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="flasktodo",
     version="0.0.1",
-    author="Zach Fedor",
-    author_email="zachfedor@gmail.com",
-    url="https://github.com/ts-cset/flask-todo",
+    authors="Steve Eckles and Kevin Lueke",
+    ##author_email="zachfedor@gmail.com",
+    url="https://github.com/kevinlueke/flask-todo",
     description="A simple to-do application",
     long_description=long_description,
     long_description_content_type="text/markdown",
