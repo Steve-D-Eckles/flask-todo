7e55e9ea140974b237eb493e2cf92cd82a7287e9
Steve Eckles <steve.d.eckles@gmail.com>
2020-03-30 15:44:19 -0400

    Add toggle functionality

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,13 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from . import db
 
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+@bp.route("/", methods=('GET', 'POST'))
 def index():
     """View for home page which shows list of to-do items."""
+
+    if request.method == 'POST':
+        with db.get_db() as con:
+            with con.cursor() as cur:
+                for item in request.form:
+                    cur.execute("""
+                        SELECT completed FROM todos
+                        WHERE id = %s
+                    """, (request.form[item],))
+                    toggle = cur.fetchone()[0]
+                    cur.execute("""
+                        UPDATE todos
+                        SET completed = %s
+                        WHERE id = %s
+                    """, (False if toggle else True, request.form[item]))
+
 
     cur = db.get_db().cursor()
     cur.execute('SELECT * FROM todos')
@@ -15,4 +31,3 @@ def index():
     cur.close()
 
     return render_template("index.html", todos=todos)
-
