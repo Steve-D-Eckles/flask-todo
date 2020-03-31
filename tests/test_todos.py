8ab8db2d90e2529d7dd7b5a0e06a2e67837778ed
Steve Eckles <steve.d.eckles@gmail.com>
2020-03-31 12:42:20 -0400

    Write tests for adding and toggling

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -10,3 +10,18 @@ def test_todo_list(client):
     assert response.data.count(b'<li class="">') == 2
     assert response.data.count(b'<li class="completed">') == 1
 
+    # Should be able to post a new, uncompleted item to the list
+    response = client.post('/', data={'new-item': 'sweep'})
+    assert response.data.count(b'<li class="">') == 3
+    assert b'sweep' in response.data
+
+def test_toggle(client):
+    # User should be able to post checked boxes to toggle completed status
+    response = client.post('/toggle', data={'1': '1'})
+
+    # User should be redirected to index after toggle is complete
+    assert 'http://localhost/' == response.headers['Location']
+
+    # Number of completed items on index page should be incremented
+    response = client.get('/')
+    assert response.data.count(b'<li class="completed">') == 2
