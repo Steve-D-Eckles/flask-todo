a6b6ba78ec2cb94a6576c493eb66b1f88377cb5d
Steve Eckles <steve.d.eckles@gmail.com>
2020-03-31 16:25:17 -0400

    Add auth functionality

--- flasktodo/schema.sql
+++ flasktodo/schema.sql
@@ -6,15 +6,20 @@
 
 -- Drop existing tables
 DROP TABLE IF EXISTS todos;
--- Add query to drop users table here
+DROP TABLE IF EXISTS users;
 
--- Add query to create users table here
-
+-- Registered App Users
+CREATE TABLE users (
+    id bigserial PRIMARY KEY,
+    username varchar(100) NOT NULL,
+    password varchar(128) NOT NULL
+);
 -- To-Do Items
 CREATE TABLE todos (
     id bigserial PRIMARY KEY,
     description varchar(140) NOT NULL,
     completed boolean NOT NULL,
-    created_at timestamp with time zone NOT NULL
-    -- Add Foreign Key to users table here
+    created_at timestamp with time zone NOT NULL,
+    --create a relationship between users and todo items
+    user_id bigint REFERENCES users(id)
 );
